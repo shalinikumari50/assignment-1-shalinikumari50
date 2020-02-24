1ff821ec0ebe20434d2ccc0974da879ba6c486d6
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-02-24 18:35:50 +0530

    add parameterized constructor : Person(String firstName, String lastName, ContactList contactList, String emailAddress)

--- src/person/Person.java
+++ src/person/Person.java
@@ -7,4 +7,11 @@ public class Person {
     private String lastName;
     private ContactList contactList;
     private String emailAddress;
+
+    public Person(String firstName, String lastName, ContactList contactList, String emailAddress) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.contactList = contactList;
+        this.emailAddress = emailAddress;
+    }
 }
