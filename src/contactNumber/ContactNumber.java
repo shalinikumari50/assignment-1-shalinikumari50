de1ce9031fb4bc9fcc70300b401a5ca13f1a3aad
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-02-24 18:19:28 +0530

    add parameterized constructor : ContactNumber(contactNumber:String)

--- src/contactNumber/ContactNumber.java
+++ src/contactNumber/ContactNumber.java
@@ -2,4 +2,8 @@ package contactNumber;
 
 public class ContactNumber {
     private String contactNumber;
+
+    public ContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
 }
