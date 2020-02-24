711eed724f68bfc7feaf435faa270394d45bbca4
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-02-24 18:21:59 +0530

    add static object : validInputs of class : ValidInputs

--- src/contactList/ContactList.java
+++ src/contactList/ContactList.java
@@ -2,6 +2,8 @@ package contactList;
 
 import contactNumber.ContactNumber;
 import myLinkedList.MyLinkedList;
+import validInputs.ValidInputs;
 
 public class ContactList extends MyLinkedList<ContactNumber> {
+    private static ValidInputs validInputs;
 }
