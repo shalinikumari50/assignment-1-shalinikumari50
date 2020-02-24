a4458b73cfa1b47c65960624aabf50168b7943d3
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-02-24 18:35:01 +0530

    add variable : firstName(String), lastName(String), contactList(ContactList), emailAddress(String)

--- src/person/Person.java
+++ src/person/Person.java
@@ -1,4 +1,10 @@
 package person;
 
+import contactList.ContactList;
+
 public class Person {
+    private String firstName;
+    private String lastName;
+    private ContactList contactList;
+    private String emailAddress;
 }
