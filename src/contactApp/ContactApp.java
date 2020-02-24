9e50fb8c347bfe6c2c35fb405da73eb5380afa1d
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-02-24 18:44:49 +0530

    add method : enterName(String) to take name as input from user

--- src/contactApp/ContactApp.java
+++ src/contactApp/ContactApp.java
@@ -16,4 +16,31 @@ public class ContactApp {
     public ContactApp() {
         personMyLinkedList = new MyLinkedList<>();
     }
+
+    public String enterName() {
+        while (true) {
+            System.out.println("Please enter the name of the Person");
+            System.out.println("First Name: ");
+            String firstName = validInputs.inputValidName();
+            if (firstName == null) {
+                return null;
+            }
+            System.out.println("Last Name: ");
+            String lastName = validInputs.inputValidName();
+            if (lastName == null) {
+                return null;
+            }
+            if (personMyLinkedList.matchFound(firstName + " " + lastName)) {
+                System.out.println("Contact with this name already exists!");
+                System.out.println("To try again with a different name, enter 't' or to return to menu, enter 'q'");
+                char choice = validInputs.inputValidChoice('q', 't');
+                if (choice == 'q') {
+                    return null;
+                } else {
+                    continue;
+                }
+            }
+            return firstName + " " + lastName;
+        }
+    }
 }
