2692dff95353e0fe10efb52a1c7af9b6f0cdb6a0
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-02-24 17:49:35 +0530

    add method : insertInAlphabeticOrder(newNode:Node<T>)(int) to add a node alphabetically and return the position at which it is added

--- src/myLinkedList/MyLinkedList.java
+++ src/myLinkedList/MyLinkedList.java
@@ -16,4 +16,22 @@ public class MyLinkedList<T> {
     public void setHead(Node<T> head) {
         this.head = head;
     }
+
+    public int insertInAlphabeticOrder(Node<T> newNode) {
+        int position = 1;
+        if (head == null || head.toString().toLowerCase().compareTo(newNode.toString().toLowerCase()) > 0) {
+            newNode.setNext(head);
+            setHead(newNode);
+            return position;
+        } else {
+            Node<T> temp = head;
+            while (temp.getNext() != null && temp.getNext().toString().toLowerCase().compareTo(newNode.toString().toLowerCase()) <= 0) {
+                position++;
+                temp = temp.getNext();
+            }
+            newNode.setNext(temp.getNext());
+            temp.setNext(newNode);
+        }
+        return ++position;
+    }
 }
